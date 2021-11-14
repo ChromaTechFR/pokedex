cf81890684c9d3854023197f41fff5b88dba560c
ChromaTechFR <kileur42680@gmail.com>
2021-11-14 13:38:13 +0100

    pokemon image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require('discord.js');
+const fetch = require('node-fetch');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require('dotenv').config()
 
@@ -6,12 +7,32 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (message) => {
-    console.log(message)
-    if (message.content === "_ping") {
-        message.reply({
-            content: "pong"
-        });
+client.on("messageCreate", async (message) => {
+    const args = message.content.split(" ")
+    if (args[0] === "-pokemon") {
+        if (args[1]) {
+
+            const query = args[1]
+
+            try {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+                const pokemon = await res.json();
+                console.log(pokemon)
+                const formatedIndex = ("00" + pokemon.id).slice(-3);
+                const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formatedIndex}.png`;
+                pokemon.image = image;
+
+                message.reply({
+                    content: pokemon.image
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            message.reply({
+                content: "aucun argument donné"
+            })
+        }
     }
 })
 
